15f222768899553007d925a098ae8d061819d4e6
KU-onji <mgr_127@yahoo.co.jp>
2024-05-15 17:16:40 +0900

    add formatting

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import re
 
-from utils import create_app_socketModeHandler, read_txt
+from utils import create_app_socketModeHandler, format_to_mrkdwn, read_txt
 
 app, handler = create_app_socketModeHandler()
 
 
-@app.message(keyword=re.compile(r"^test$"))
+@app.message(re.compile(r"^testApp$"))
 def post_test_message(body):
     main_event = body["event"]
     channel = main_event["channel"]
     summary = read_txt("pdfs/summary.txt")
+    summary_formatted = format_to_mrkdwn(summary)
     app.client.chat_postMessage(
-        channel=channel, text=summary, blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": summary}}]
+        channel=channel,
+        text=summary,
+        thread_ts=main_event["ts"],
+        blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": summary_formatted}}],
     )
+    print("Message from testApp.")
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,33 @@ def create_app_socketModeHandler():
 def read_txt(file_path):
     with open(file_path, "r", encoding="utf-8-sig") as f:
         return f.read()
+
+
+def format_to_mrkdwn(text: str) -> str:
+    def pre_format(line: str) -> str:
+        return line.replace("*", "_")
+
+    def format_list(line: str) -> str:
+        if line.startswith("- ") and len(line) > 2:
+            return f"・ {line[2:]}"
+        else:
+            return line
+
+    def format_bold(line: str) -> str:
+        return line.replace("__", "*")
+
+    def format_section(line: str) -> str:
+        if line.startswith("### ") and len(line) > 4:
+            line = line.replace(":\n", "")
+            return f"*{line[4:]}*:\n"
+        else:
+            return line
+
+    li_line = list(map(pre_format, text.splitlines(keepends=True)))
+    res = []
+    for line in li_line:
+        line_formatted = format_list(line)
+        line_formatted = format_bold(line_formatted)
+        line_formatted = format_section(line_formatted)
+        res.append(line_formatted)
+    return "".join(res)
